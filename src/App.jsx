dd1a15c1710ef0c41dbe1c4be70d70a3efa06c9b
Andrew Lyzenga <darthmaul85@hotmail.com>
2018-10-10 15:42:55 -0700

    update state

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      taiko: {},
+      stands: {},
+      percussion: {}
+    }
+  }
   render() {
     return (
       <div className="App">
