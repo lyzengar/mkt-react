67c46b381481a06bff81da458cc818f734962ae3
Andrew Lyzenga <darthmaul85@hotmail.com>
2018-10-12 14:09:36 -0700

    add functions

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,11 @@ class App extends Component {
       percussionURL: {}
     }
   }
+
+  handleTaiko = () => {
+    this.setState({ taiko: {} })
+  }
+
   render() {
     return (
       <div className="App">
